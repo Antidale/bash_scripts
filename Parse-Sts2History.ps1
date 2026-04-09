cc77e2b59f744ca3ce1efed5bd392a556681979f
Antidale <antidale@tellah.life>
2026-04-08 20:07:44 -0700

    Updates to make this run on powershell 5

--- Parse-Sts2History.ps1
+++ Parse-Sts2History.ps1
@@ -3,9 +3,18 @@ param(
     [int] $minAscension = 0
 )
 
+### CHANGE THIS VALUE TO YOUR STEAM ID.
+# on windows that's at C:\Program Files (x86)\Steam\userdata\ and will be a folder there. If your account is the only one there, great! you're done looking, just copy that string of digits and overwrite the 0 below. If you share a computer with someone else who has a different account, I **think** there should be seperate folders, but don't actually know.
+$steamId = 0
+
 $csvName = "STS2_Run_Summary.csv"
 $summaryCsvName = "STS2_Data_Summary.csv"
 $desiredAscenions = $minAscension..10 | ForEach-Object { $PSItem.ToString() }
+
+if ($steamId -eq 0) {
+    Write-Host "You must set the steamId in the script"
+    return
+}
 
 function Initialize-DataStorage {
     
@@ -36,11 +45,15 @@ function Get-HistoryPath {
     
     $tempPath = ''
     if ($IsWindows) {
-        $tempPath = "C:\Program Files (x86)\Steam\userdata\36517658\2868840\remote\$profileName\saves\history"
+        $tempPath = "C:\Program Files (x86)\Steam\userdata\$steamId\2868840\remote\$profileName\saves\history"
     }
     
     if ($IsMacOS) {
-        $tempPath = "~/Library/Application Support/Steam/userdata/36517658/2868840/remote/$profileName/saves/history"
+        $tempPath = "~/Library/Application Support/Steam/userdata/$steamId/2868840/remote/$profileName/saves/history"
+    }
+
+    if (($tempPath -eq '') -and (!$IsLinux)) {
+        $tempPath = "C:\Program Files (x86)\Steam\userdata\$steamId\2868840\remote\$profileName\saves\history"
     }
 
     return $tempPath
@@ -55,27 +68,27 @@ if ($savedRuns -And $savedRuns.Data.Length -and $savedRuns.Data.Length -GT 0) {
     $lastAccess = $savedRuns.LastWrite
 }
 
-Get-ChildItem -Path $stsPath 
-| Where-Object -Property LastWriteTime -GT $lastAccess 
-| ForEach-Object -Parallel {
-    $json = Get-Content $_ 
-    | ConvertFrom-Json 
-    | Where-Object { $_.players.Count -eq 1 } 
-    | Select-Object ascension, win, build_id -ExpandProperty players
-    | Select-Object -Property ascension, character, win, relics, build_id 
+$relevantFiles = Get-ChildItem -Path $stsPath | Where-Object -Property LastWriteTime -GT $lastAccess
 
-    if ($json) {
-        [PSCustomObject] @{
-            File      = $_.Name
-            Ascension = $json.ascension
-            Result    = $json.win ? "Win" : "Loss"
-            Character = (Get-Culture).TextInfo.ToTitleCase($json.character.Substring($json.character.IndexOf('.') + 1).ToLower())
-            Version   = $json.build_id
-            Relics    = $json.Relics | Select-Object -ExpandProperty id | Join-String { (Get-Culture).TextInfo.ToTitleCase($_.Substring($_.IndexOf('.') + 1 ).ToLower()).Replace('_', ' ') } -Separator ', '
-        } 
-    }
-} -ThrottleLimit 4
-| Export-Csv -Path $savedRuns.Path -Append -NoTypeInformation
+$(foreach ($file in $relevantFiles) {
+        $json = Get-Content -Path $file.FullName | ConvertFrom-Json | Where-Object { $_.players.Count -eq 1 } | Select-Object ascension, win, build_id -ExpandProperty players | Select-Object -Property ascension, character, win, relics, build_id 
+
+        if ($json) {
+            $relics = $json.Relics | Select-Object -ExpandProperty id
+
+            [PSCustomObject] @{
+                File      = $_.Name
+                Ascension = $json.ascension
+                Result    = If ($json.win) { "Win" } Else { "Loss" }
+                Character = (Get-Culture).TextInfo.ToTitleCase($json.character.Substring($json.character.IndexOf('.') + 1).ToLower())
+                Version   = $json.build_id
+                Relics    = $(foreach ($relic in $relics) {
+                        (Get-Culture).TextInfo.ToTitleCase($relic.Substring($relic.IndexOf('.') + 1 ).ToLower()).Replace('_', ' ')
+                    }) -join ", "
+            }
+
+        }
+    } ) | Export-Csv -Path $savedRuns.Path -Append -NoTypeInformation
 
 Import-Csv -Path $savedRuns.Path  | Where-Object { $_.Ascension -in $desiredAscenions } | Group-Object -Property Ascension, Character, Result -NoElement | ForEach-Object {
     $summary = $_.Name.Split(', ')
